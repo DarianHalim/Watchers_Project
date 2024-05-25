2a30fb28b0baa37af55488c747bf2236780b2d9c
Darian.H <darian.live125@gmail.com>
2024-05-25 14:26:52 +0700

    Finished Home Page
    Home Page Done

---
+++ Project/js/homeScroll.js
@@ -0,0 +1,28 @@
+// HomeScroll
+let pointerHome = 0;
+let elementHome = 0;
+
+
+const scrollableHome = document.querySelector('.featuredRow');
+
+const onDragHome = (event) => {
+    if (pointerDown && event.pointerType === 'mouse') {
+        scrollableHome.scrollLeft = elementHome - event.clientX + pointerHome;
+    }
+};
+
+scrollableHome.addEventListener('pointerdown', (event) => {
+    if (event.pointerType === 'mouse') {
+        pointerDown = true;
+        pointerHome = event.clientX;
+        elementHome = scrollableHome.scrollLeft;
+        document.addEventListener('pointermove', onDragHome);
+    }
+});
+
+document.addEventListener('pointerup', (event) => {
+    if (event.pointerType === 'mouse') {
+        pointerDown = false;
+        document.removeEventListener('pointermove', onDragHome);
+    }
+});
